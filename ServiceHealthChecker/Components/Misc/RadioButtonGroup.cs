c3db13781978cd32bb8d79b1cc1be51121066b2c
Jakub Orsula <orsula.jakub@gmail.com>
2020-07-11 06:30:14 +0100

    visual changes

---
+++ ServiceHealthChecker/Components/Misc/RadioButtonGroup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ServiceHealthChecker.Components.Misc
+{
+    public partial class RadioButtonGroup<T>
+    {
+        [Parameter]
+        public HttpMethods SelectedItem { get; set; }
+
+        [Parameter]
+        public IEnumerable<T> Items { get; set; }
+
+        [Parameter]
+        public EventCallback<T> SelectItem { get; set; }
+    }
+}
